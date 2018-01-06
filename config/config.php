0695b258461b9b29bd95f4de2e2cea8deb6c397f
rakib420086 <35164131+rakib420086@users.noreply.github.com>
2018-01-07 00:07:20 +0600

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "db_blog");
+define("TITLE", "Training with live project");
+define("KEYWORDS", "Html tutorial, PHP tutorial, Java tutorial, c++ Tutorial");
+define("DESCRIPTION", "This is a Java learning website., This is a Html learning website., This is a CSS learning website, This is a PHP learning website.");
+?>
+
